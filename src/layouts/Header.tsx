b4286af6c947b13f6d4303a2520606ac157f27ad
hait2810 <ngvhai.dev@gmail.com>
2024-09-03 19:45:28 +0700

    update

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -53,23 +53,24 @@ const Header = () => {
         <nav className={`hidden max-lg:block bg-white h-[100vh] absolute top-20 bottom-0 left-0 ${isOpenMenu ? 'right-0 opacity-100' : 'right-[1000px] opacity-0'} right-0 border-t-2 border-blue-300 transition-all ease-in duration-300`}>
           <ul className="container mx-auto px-4 pt-4 flex flex-col  gap-2 text-xl font-normal transition-all ease-in duration-500">
             <li>
-              <Link className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Trang chủ</Link>
+              <Link onClick={() => setIsOpenMenu(false)} className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Trang chủ</Link>
             </li>
             <li>
-              <Link className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Nam</Link>
+              <Link onClick={() => setIsOpenMenu(false)} className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Nam</Link>
             </li>
             <li>
-              <Link className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Nữ</Link>
+              <Link onClick={() => setIsOpenMenu(false)} className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Nữ</Link>
             </li>
             <li>
-              <Link className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Sản phẩm khuyến mãi</Link>
+              <Link onClick={() => setIsOpenMenu(false)} className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="">Sản phẩm khuyến mãi</Link>
             </li>
             <li>
-              <Link className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="/intro">Giới thiệu</Link>
+              <Link onClick={() => setIsOpenMenu(false)} className="cursor-pointer  hover:bg-blue-300 px-2 py-1 hover:rounded-full hover:text-white duration-500" to="/intro">Giới thiệu</Link>
             </li>
             <li>
               <div className="mt-4">
                 <Link
+                  onClick={() => setIsOpenMenu(false)}
                   to="#"
                   className="rounded-full bg-[#f2f4f7] px-6 py-3 text-xl text-black font-normal tracking-[2px] hover:bg-black hover:text-white flex items-start gap-x-1 w-fit"
                 >
